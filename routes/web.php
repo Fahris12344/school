de17bf8827737502f83f4e42edc63aacc96d1526
Fahris12344 <farismhd255@gmail.com>
2024-10-18 13:20:52 +0700

    hhgbybyy

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/1', function () {
+    return view('home');
+});
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
